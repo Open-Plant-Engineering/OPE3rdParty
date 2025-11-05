4fb3bbed66964cacce7429ee338c368a9a841a4a
Shivang Kheradiya <shivangatul@gmail.com>
2025-11-05 12:55:48 +0100

    OCCT Build Code Added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OPE3rdParty
+
+All the Thridparty Open source Repository are built using this repository for OPE Modules.
+
+# Reference Repositories:
+
+- [OCCT](https://git.dev.opencascade.org/repos/occt.git)
---
+++ cMake/GitOCCT.cmake
@@ -0,0 +1,15 @@
+include(ExternalProject)
+
+set(OCC_INSTALL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Install CACHE PATH "OpenCASCADE install directory")
+
+ExternalProject_Add(OpenCASCADE
+    GIT_REPOSITORY https://git.dev.opencascade.org/repos/occt.git
+    GIT_TAG master
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/download/occt
+    CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=Debug
+        -D3RDPARTY_DIR=${OCC3RDPARTY_DIR}
+        -DINSTALL_DIR=${OCC_INSTALL_DIR}/OCCT/Install
+    BUILD_ALWAYS 1
+    INSTALL_DIR ${OCC_INSTALL_DIR}/OCCT/Install
+)
