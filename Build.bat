e775b28323ceae8774f73b3ea06ff95625b468d6
shivangKheradiya <shivangatul@gmail.com>
2025-11-15 17:29:03 +0100

    OCCT Installation Cmake Project Converted to Bat File

---
+++ Build.bat
@@ -0,0 +1,29 @@
+set OPE3rdParty_DIR=%cd%
+
+rem setup by user
+set OCC3RDPARTY_DIR=E:/SKGitRepo/OpenCascade/3rdParty
+E:
+rem setup by user
+
+rem Automatic Download process for OCCT
+echo Cloning OCCT repository...
+git clone https://github.com/Open-Cascade-SAS/OCCT.git "%OPE3rdParty_DIR%/Download/OCCT"
+cd "%OPE3rdParty_DIR%/Download/OCCT"
+git pull
+git checkout master
+
+echo Configuring Debug build...
+mkdir "%OPE3rdParty_DIR%/Download/OCCT/Build"
+cmake -S "%OPE3rdParty_DIR%/Download/OCCT" -B "%OPE3rdParty_DIR%/Download/OCCT/Build" -D3RDPARTY_DIR=%OCC3RDPARTY_DIR% -DCMAKE_BUILD_TYPE=Debug -DINSTALL_DIR=%OPE3rdParty_DIR%/Install/OCCT 
+
+rem set MSBuild_DIR=C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin
+rem If required generate using below script
+rem "%MSBuild_DIR%/MSBuild.exe" OCCT.sln /p:Configuration=Debug /p:Platform="x64"
+rem If required generate using below script
+rem "%MSBuild_DIR%/MSBuild.exe" OCCT.sln /p:Configuration=Release /p:Platform="x64"
+
+echo Building & Installing Debug and Release...
+cmake --build "%OPE3rdParty_DIR%/Download/OCCT/Build" --target INSTALL --config Debug
+cmake --build "%OPE3rdParty_DIR%/Download/OCCT/Build" --target INSTALL --config Release
+
+
